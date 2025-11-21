51ce958fdbcfddfbb7ab359faef8128a2a3eb317
KHKAmuta <rudgnsrb888@naver.com>
2025-11-21 13:04:42 +0900

    change

--- Assets/6_Script/EnemyManager.cs
+++ Assets/6_Script/EnemyManager.cs
@@ -4,26 +4,45 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    public static EnemyManager Instance; // 싱글톤 Instance화
-    [SerializeField] GameObject enemyPrefab; // 적의 프리펩
+    public static EnemyManager Instance; // 싱글톤 인스턴스
+    [SerializeField] GameObject enemyPrefab; // 적 프레팝
     [SerializeField] float spawnTime; // 생성 시간
-    [SerializeField] Transform[] waypoints; // 이동 위치를 배열로
-    public Transform[] WayPoints => waypoints; // 이동 위치 배열 프로퍼티
+    [SerializeField] Transform[] waypoints; // 이동 위치 배열
+    List<Enemy> enemyList; // 생성된 적 리스트
+
+    public Transform[] Waypoints => waypoints; // 이동위치배열 프로퍼티
+    public List<Enemy> EnemyList => enemyList; // 적리스트 프로퍼티
+
     void Awake()
     {
-        if(Instance == null)
+        if (Instance == null) Instance = this;
+    }
+
+    // Start는 코루틴으로 사용할 수 있다
+    IEnumerator Start()
+    {
+        // 생성된 적 리스트 초기화
+        enemyList = new List<Enemy>();
+
+        while (true)
         {
-            Instance = this;
+            // 적 프레팝으로 오브젝트를 생성하고 Enemy 스크립트 연결
+            Enemy enemy = Instantiate(enemyPrefab, 
+                transform).GetComponent<Enemy>();
+            // 적 초기화
+            enemy.Init();
+            // 적을 리스트에 넣기
+            enemyList.Add(enemy);
+            // 생성 시간 기다렸다가 다음 적 생성
+            yield return new WaitForSeconds(spawnTime);
         }
     }
 
-    // Start는 코루틴으로 사용할 수 있다.
-    IEnumerator Start()
+    public void DestroyEnemy(Enemy enemy)
     {
-        while(true)
-        {
-            // 적의 프리펩으로 오브젝트를 생성하고 Enemy 스크립트를 연결
-            Enemy enemy = Instantiate(enemyPrefab, transform).GetComponent<Enemy>();
-        }
+        // 적리스트에서 지정한 적 지우기
+        enemyList.Remove(enemy);
+        // 적 오브젝트 삭제
+        Destroy(enemy.gameObject);
     }
 }
