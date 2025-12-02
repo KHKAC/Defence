7b15d9749b467ba365c16e5ed80e76bb85aa3103
KHKAC <rudgnsrb888@naver.com>
2025-12-02 14:32:55 +0900

    251202 WaveManager에 JsonUtility 추가

--- Assets/6_Script/WaveSystem.cs
+++ Assets/6_Script/WaveSystem.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 // 유니티에디터에서 사용하거나 파일로 저장하기 위해서 시리얼라이즈
 [System.Serializable]
 public struct Wave
 {
+    [Tooltip("고정으로 할 경우 적 최대 수와 프리펩의 총 개수를 맞춰줘야한다")]
+    public bool isStatic;                   // 고정인지 랜덤인지
     [Tooltip("적 생성 주기 : 작을수록 빨리 생성됨")]
-    public float            spawnTime;      // 적 생성 주기
+    public float spawnTime;                 // 적 생성 주기
     [Tooltip("적 최대 수 : 이번 웨이브에 나오는 적의 수")]
-    public int              maxEnemyCount;  // 적 최대 숫자
-    public GameObject[]     enemyPrefabs;   // 적 종류    
+    public int maxEnemyCount;               // 적 최대 숫자
+    public GameObject[] enemyPrefabs;       // 적 종류    
+    public float[] spawnTimeStatic;         // 고정일 때 적의 생성 주기
 }
 
+// Json 사용을 위한 wrapper class
+public class WaveWrapper
+{
+    public Wave[] waveArray;
+}
 public class WaveSystem : MonoBehaviour
 {
+    const string FILE_NAME = "waves.json";
     [SerializeField] Wave[] waves; // 웨이브 배열
     int currentWaveIndex = -1; // 현재 웨이브 인덱스 (0에서 시작해야해서 초기값은 -1)
 
@@ -41,5 +51,28 @@ public class WaveSystem : MonoBehaviour
     public string GetWaveInfoString()
     {
         return $"{currentWaveIndex + 1}\n--\n{waves.Length}";
+    }
+
+    // 에디터 상단 메뉴가 아닌 WaveSystem이 붙어있는 컴포넌트에서 메뉴가 열린다
+    [ContextMenu("DefenceTower/Make Json data", false, 1)]
+    public void MakeJsonData()
+    {
+        // 저장 데이터가 배열인 경우 wrapper class로 한 번 감싸주자
+        WaveWrapper data = new WaveWrapper();
+        data.waveArray = waves;
+        string jsonData = JsonUtility.ToJson(data, true);
+        string path = Path.Combine(Application.dataPath, FILE_NAME);
+        File.WriteAllText(path, jsonData);
+        Debug.Log("Make Json data is done.");
+    }
+
+    [ContextMenu("DefenceTower/Load from Json", false, 2)]
+    public void LoadFromJson()
+    {
+        string path = Path.Combine(Application.dataPath, FILE_NAME);
+        string jsonData = File.ReadAllText(path);
+        var json = JsonUtility.FromJson<WaveWrapper>(jsonData);
+        waves = json.waveArray;
+        Debug.Log("Loaded Json data from file");
     }
 }
